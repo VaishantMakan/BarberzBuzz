b933ff43a55756da1a64c64e4def2cf7f5dececf
Vaishaant Makkan <vaishantmakan@gmail.com>
2022-07-18 03:07:57 +0530

    Sliding Card Carousel added
    middle-section-updated , owl carousel used, stylish heading designed

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Navbar JS .... scrolling down turns navbar dark 
 var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function(){
@@ -5,5 +6,24 @@ window.addEventListener('scroll', function(){
         nav.classList.add('bg-dark', 'shadow');
     } else {
         nav.classList.remove('bg-dark', 'shadow');
+    }
+})
+
+// sliding cards JS ... owl carousel
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    stagePadding: 100,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
     }
 })
